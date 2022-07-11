984e722d49afcc2f2d14cce8563024196fdb89ff
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-07-11 18:12:33 +0200

    Mostrati i badge dei tag nella show

--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
 <div class="container">
-    <h1>{{ $post->title }}</h1> <a class="btn btn-primary  " href=""> EDIT DA COLLEGARE </a>
+    <div class="d-flex justify-content-between align-items-center">
+        <h1>{{ $post->title }}</h1> <a class="btn btn-primary" href="{{ route('admin.posts.edit', $post) }}"> EDIT </a>
+    </div>
+    <h5>Tags:</h5>
+    @if ($post->tags)
+        @foreach ($post->tags as $tag)
+        <span class="badge badge-success">{{ $tag->name }}</span>
+        @endforeach
+    @endif
+    <h4 class="mt-5">Corpo del post:</h4>
     <p>{{ $post->text }}</p>
-    <a class="btn btn-secondary mt-5" href="{{ route('admin.posts.index') }}"> << INDIETRO </a>
+    <a class="btn btn-secondary mt-3" href="{{ route('admin.posts.index') }}"> << INDIETRO </a>
 </div>
 @endsection
